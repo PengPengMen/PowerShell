060b35516ceec0b43d7bac56c60a2032c87c9e67
menpengpeng <menpengpeng@163.com>
2017-11-18 14:21:24 +0800

    SQLserver数据库备份测试脚本

---
+++ SQLServer数据库备份.ps1
@@ -0,0 +1,14 @@
+﻿# Get-ExecutionPolicy -List  
+# Set-ExecutionPolicy Unrestricted -Scope CurrentUser  
+# -CompressionOption [Default / On / Off] (是否压缩)  
+# -BackupAction [Database / Log / Files] (备份类型)  
+# -Incremental (差异备份)  
+  
+import-module sqlps;  
+$instance = '192.168.2.225,8033'  
+$database = 'Test'  
+$targetDir = 'E:\Backup\TestBackup'  
+$datestr = Get-Date -format yyyyMMddHHmmss  
+$backupFile = $targetDir + '\' + $database + '_' + $datestr + '.bak'  
+  
+Backup-SqlDatabase -ServerInstance $instance -Database $database -CompressionOption On -Checksum -BackupFile $backupFile  
